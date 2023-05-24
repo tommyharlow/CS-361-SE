30572c17a84d0ac0c76e12101c4248434d7145c8
Sonny Box <sonny@nexttechconsultants.com>
2023-05-24 10:18:30 -0700

    Added progress bar and other time updates

--- preload.js
+++ preload.js
@@ -10,23 +10,63 @@ window.addEventListener('DOMContentLoaded', () => {
         replaceText(`${dependency}-version`, process.versions[dependency])
     }
 
-    // end of old code
+    // Start of MP3 Player logic
 
     const loadMusicDirButton = document.getElementById('load-music-dir')
     const pausePlayButton = document.getElementById('pause-play')
     const nextButton = document.getElementById('next')
     const prevButton = document.getElementById('prev')
     const repeatButton = document.getElementById('repeat')
+    const progressBar = document.getElementById('progress-bar')
+    const progressBarContainer = document.getElementById('progress-bar-container')
     const audioPlayer = document.getElementById('audio-player')
+    const timeElapsed = document.getElementById('time-elapsed')
+    const timeToggleButton = document.getElementById('time-toggle')
 
+    let updateInterval = null
     let musicFiles = []
     let currentFileIndex = 0
     let songLoaded = false
-    let songRepeat = false
+    let songRepeat = 'off'
     let prevButtonClickedAt = 0
+    let displayRemainingTime = false
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60)
+        const seconds = Math.floor(time % 60)
+        return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+    }
+
+    const updateTimeAndProgressBar = () => {
+        if(audioPlayer.paused) return;
+
+        const percentage = (audioPlayer.currentTime / audioPlayer.duration) * 100
+        progressBar.style.width = `${percentage}%`
+        timeElapsed.innerText = formatTime(audioPlayer.currentTime)
+
+        if (displayRemainingTime) {
+            const remainingTime = audioPlayer.duration - audioPlayer.currentTime
+            timeToggleButton.innerText = formatTime(remainingTime)
+        } else {
+            timeToggleButton.innerText = formatTime(audioPlayer.duration)
+        }
+    }
+
+    timeToggleButton.addEventListener('click', () => {
+        displayRemainingTime = !displayRemainingTime
+    })
+
+    updateInterval = setInterval(updateTimeAndProgressBar, 1000)
 
     loadMusicDirButton.addEventListener('click', () => {
         ipcRenderer.send('open-music-dir')
+    })
+
+    progressBarContainer.addEventListener('click', (e) => {
+        if (songLoaded) {
+            const percentage = e.offsetX / progressBarContainer.offsetWidth
+            audioPlayer.currentTime = audioPlayer.duration * percentage
+        }    
     })
 
     pausePlayButton.addEventListener('click', () => {
@@ -80,22 +120,57 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 
     audioPlayer.addEventListener('ended', () => {
-        if (songRepeat) {
-            audioPlayer.currentTime = 0 // Restart the song to the beginning
-            audioPlayer.play() // Just in case the song doesn't restart, unneeded
+        switch (songRepeat) {
+            case 'off':
+                // If it's the last song in the playlist, stop playing
+                if (currentFileIndex + 1 === musicFiles.length) {
+                    audioPlayer.pause();
+                    songLoaded = false; // Set the song as unloaded
+                    progressBar.style.width = '0'; // Reset the progress bar
+                    return; // End the function here
+                }
+
+                // If it's not the last song, go to the next one
+                currentFileIndex++;
+                break;
+            case 'one':
+                // Restart the current song
+                audioPlayer.currentTime = 0;
+                break;
+            case 'loop':
+                // Go to the next song, looping back to the start if it's the last one
+                currentFileIndex = (currentFileIndex + 1) % musicFiles.length;
+                break;
         }
-    })
+
+        // Set and play the new song, and reset the progress bar
+        audioPlayer.src = musicFiles[currentFileIndex];
+        audioPlayer.play();
+        progressBar.style.width = '0';
+    });
+
 
     repeatButton.addEventListener('click', () => {
-        songRepeat = !songRepeat // Toggle the variable
-        repeatButton.textContent = songRepeat ? "Repeat On" : "Repeat Off" // Change the text of the button
-    })
+        switch (songRepeat) {
+            case 'off':
+                songRepeat = 'one';
+                break;
+            case 'one':
+                songRepeat = 'loop';
+                break;
+            case 'loop':
+                songRepeat = 'off';
+                break;
+        }
+        repeatButton.textContent = `Repeat: ${songRepeat}`; // Change the text of the button
+    });
 
     ipcRenderer.on('loaded-music-dir', (event, files) => {
         musicFiles = files
         currentFileIndex = 0
         songLoaded = false // Reset this variable when a new directory is loaded
-    })
-
-
+        progressBar.style.width = '0'
+        clearInterval(updateInterval)
+        updateInterval = setInterval(updateTimeAndProgressBar, 1000)
+    });
 })
